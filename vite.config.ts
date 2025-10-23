e6b2f539ba5c24fa725273c3c1b286c24fccf270
tartar008 <127741947+tartar008@users.noreply.github.com>
2025-10-23 19:58:32 +0000

    Deploy latest build to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "./", // 👈 ต้องเป็นชื่อ repo
+  base: "/AlumniGallery/", // 👈 ต้องเป็นชื่อ repo
   plugins: [react()],
   resolve: {
     alias: {
